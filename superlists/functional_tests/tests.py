0020c6aa640e91414a6fab9196bff363e6ce3c17
Alipqb <ali.pirqarehbaghi@yahoo.com>
2021-06-29 12:52:07 +0430

    new URL + view for adding to existing lists. FT passes :-)

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -6,15 +6,15 @@ import time
 
 MAX_WAIT = 10
 
-browser = webdriver.Firefox()
 
 class NewVisitorTest(LiveServerTestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self) -> None:
-        return self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
+
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -28,6 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -68,7 +69,8 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, she goes back to sleep
 
-    def test_multiple_users_can_start_lists(self):
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -80,14 +82,14 @@ class NewVisitorTest(LiveServerTestCase):
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
 
-        # Now a new user, Francis, comes along to the site
+        # Now a new user, Francis, comes along to the site.
 
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        # Fracis visits the home page. There is no sign of Edith's
+        # Francis visits the home page.  There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
@@ -95,7 +97,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('make a fly', page_text)
 
         # Francis starts a new list by entering a new item. He
-        # is less interesting than Edith ...
+        # is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
@@ -110,7 +112,5 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-        
+
         # Satisfied, they both go back to sleep
-
-
